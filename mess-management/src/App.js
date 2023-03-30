a9c7579fc9f11ef2e54b0086d1180642464d748e
crescentpartha <crescentpartha406@gmail.com>
2023-03-30 22:59:37 +0600

    Setup Routes

--- mess-management/src/App.js
+++ mess-management/src/App.js
@@ -1,12 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Authentication/Login';
+import Register from './components/Authentication/Register';
 import Home from './components/Home/Home';
+import NotFound from './components/Shared/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ mess-management/src/components/Authentication/ForgetPass.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgetPass = () => {
+    return (
+        <div>
+            <h2>Retrieve Password</h2>
+        </div>
+    );
+};
+
+export default ForgetPass;
