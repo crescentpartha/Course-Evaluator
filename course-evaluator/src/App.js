de6e58d498a8edec008cdbccdd294d111f10153e
crescentpartha <crescentpartha406@gmail.com>
2023-06-14 21:53:35 +0600

    Navbar section added

--- course-evaluator/src/App.js
+++ course-evaluator/src/App.js
@@ -4,10 +4,12 @@ import Login from './components/Authentication/Login';
 import Register from './components/Authentication/Register';
 import Home from './components/Home/Home';
 import NotFound from './components/Shared/NotFound';
+import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
     <div className="App text-secondary">
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
--- course-evaluator/src/components/Home/Home.js
+++ course-evaluator/src/components/Home/Home.js
@@ -4,19 +4,6 @@ const Home = () => {
     return (
         <div>
             <h2>Home</h2>
-            <h1 className="text-3xl text-secondary font-bold underline">
-                Hello world!
-            </h1>
-            <button className='btn btn-active btn-primary text-info'>Primary</button>
-            <br />
-            <p className='text-3xl font-lato text-primary'>Primary</p>
-            <p className='text-3xl font-lobster text-secondary'>Secondary</p>
-            <p className='text-3xl font-opensans text-accent'>Accent</p>
-            <p className='text-3xl text-neutral'>Neutral</p>
-            <p className='text-3xl text-info'>Info</p>
-            <p className='text-success'>Success</p>
-            <p className='text-warning'>Warning</p>
-            <p className='text-error'>Error</p>
         </div>
     );
 };
